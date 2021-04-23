6dbbb5b52e64a8ecfd7565ffcfc2afe463aca88a
docppp <29142757+docppp@users.noreply.github.com>
2021-04-24 00:54:56 +0200

    TODO

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ if __name__ == '__main__':
     root = tk.Tk()
     app = IoursUi(root)
     if not fillTextBoxAtStartup(app.text1, app.text2, app.entry1):
+        # TODO
+        # Textbox should be cleared here, but
+        # they are disabled after removing content
         tkinter.messagebox.showinfo("No data found", "Wrong or no data found in iou.txt "
                                                      "If run this app for the first time, then it is ok.")
     app.run()
