1d2a64527aae4b402c1bc706b1c8b04568a1244e
docppp <29142757+docppp@users.noreply.github.com>
2021-06-10 21:47:52 +0200

    Startup filler refactor.

--- utils.py
+++ utils.py
@@ -1,57 +1,47 @@
+import tkinter
 
-def auto_str(cls):
-    def __str__(self):
-        return '%s(%s)' % (
-            type(self).__name__,
-            ', '.join('%s=%s' % item for item in vars(self).items())
-        )
-
-    cls.__str__ = __str__
-    return cls
+from ldr.loader_base import LoaderBase
 
 
 def fillTextBoxAtStartup(combat_box, runes_box, converge_box, runes_spin, op_spin):
+    lcs = locals()
+    boxes_to_fill = []
+    spin_frames_to_fill = []
+    for i in lcs:
+        if type(lcs[i]) is tkinter.Text or type(lcs[i]) is tkinter.ttk.Entry:
+            boxes_to_fill.append(i)
+        else:
+            spin_frames_to_fill.append(i)
+
+    text = LoaderBase().file_content
+    fill_ranges = {
+        'combat_box': (0, 9),
+        'runes_box': (9, 17),
+        'converge_box': (17, 18),
+        'spinbox_r1r': 18,
+        'spinbox_r1l': 19,
+        'spinbox_r2r': 20,
+        'spinbox_r2l': 21,
+        'spinbox_op': 22,
+    }
+
     try:
-        with open("iou.txt") as file:
-            text = file.readlines()
-
+        for box in boxes_to_fill:
             tmp = ''
-            try:
-                for i in range(9):
-                    tmp += text[i]
-            except IndexError:
-                return False
-            combat_box.insert('0.0', tmp[:-1])
+            for i in range(fill_ranges[box][0], fill_ranges[box][1]):
+                tmp += text[i]
+            start = '0.0' if type(locals()[box]) is tkinter.Text else '0'
+            locals()[box].insert(start, tmp[:-1])
 
-            tmp = ''
-            try:
-                for i in range(9, 17):
-                    tmp += text[i]
-            except IndexError:
-                return False
-            runes_box.insert('0.0', tmp[:-1])
-
-            tmp = ''
-            try:
-                for i in range(17, 18):
-                    tmp += text[i]
-            except IndexError:
-                return False
-            converge_box.insert('0', tmp[:-1])
-
-            try:
-                runes_spin.spinbox_r1r.insert('0', text[18][:-1])
-                runes_spin.spinbox_r1l.insert('0', text[19][:-1])
-                runes_spin.spinbox_r2r.insert('0', text[20][:-1])
-                runes_spin.spinbox_r2l.insert('0', text[21][:-1])
-                op_spin.spinbox_op.insert('0', text[22][:-1])
-            except IndexError:
-                return False
-
-        return True
-
-    except FileNotFoundError:
+        runes_spin.spinbox_r1r.insert('0', text[fill_ranges['spinbox_r1r']][:-1])
+        runes_spin.spinbox_r1l.insert('0', text[fill_ranges['spinbox_r1l']][:-1])
+        runes_spin.spinbox_r2r.insert('0', text[fill_ranges['spinbox_r2r']][:-1])
+        runes_spin.spinbox_r2l.insert('0', text[fill_ranges['spinbox_r2l']][:-1])
+        op_spin.spinbox_op.insert('0', text[fill_ranges['spinbox_op']][:-1])
+    except IndexError:
         return False
+
+    return True
 
 
 def saveFromTextBoxToFile(combat_box, runes_box, converge_box, runes_spin, op_spin):
